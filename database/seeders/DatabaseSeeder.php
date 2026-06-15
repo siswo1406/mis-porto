e9d836e95bbfa528d59072fe4b879055f4b8eebf
Siswo Sukmo Pamungkas <siswo@Siswos-MacBook-Pro.local>
2026-06-15 22:22:31 +0700

    feat(auth): restructure users table schema and add dummy seeder data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,13 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'SISWO SUKMO PAMUNGKAS',
+            'nik' => '1978.MTK.1222',
+            'email' => 'siswo.sukmo@ptmjl.co.id',
+            'roles' => 'admin',
+            'region' => 'MJL',
+            'unit' => 'HO',
+            'password' => \Illuminate\Support\Facades\Hash::make('@siswo1998'),
         ]);
     }
 }
