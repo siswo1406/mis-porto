00ffe71d607987c4e5fb596de2392d64a9fa6a93
Siswo Sukmo Pamungkas <siswo@Siswos-MacBook-Pro.local>
2026-06-15 22:22:31 +0700

    feat(core): configure routing redirects and authenticated session handler

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->redirectUsersTo('/home');
+
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
