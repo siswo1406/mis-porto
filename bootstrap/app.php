3d067690946b660534bd118c5bad0a8ab66c541b
Siswo Sukmo Pamungkas <siswo@Siswos-MacBook-Pro.local>
2026-06-16 15:00:04 +0700

    Fix Authentication Session and Bootstrap routing config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->redirectUsersTo('/home');
+        $middleware->redirectUsersTo('/dashboard');
 
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
